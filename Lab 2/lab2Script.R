384391f1f002fd0b8cdc06f4ef30f58e72ca7ac3
ltn7 <ltn7@sfu.ca>
2023-01-25 19:43:18 -0800

    Finished lab 1, 2, 3

--- Lab 2/lab2Script.R
+++ Lab 2/lab2Script.R
@@ -2,23 +2,25 @@
 library(tidyverse)
 
 # Read data and select
-hiv = read.csv("HIVprev.csv", stringAsFactors = FALSE)
+hiv = read.csv("HIVprev.csv", stringsAsFactors = FALSE)
 hiv = select(hiv, Country, year, prevalence)
 
-# View
+# Viewing
 head(hiv)
 tail(hiv)
 summary(hiv)
 
-# Plotting
-ggplot(data = hiv, mapping = aes(x = year, y= prevalence, group = Country, alpha = Country)) + geom_line()
-    
+# Exercise
+#1. Plot time series of HIV prevalence by year for each country
+ggplot(data = hiv, mapping = aes(x = year, y = prevalence, group_by = Country)) +
+  geom_line(alpha = 0.3)
 
-# filter data   
-cc <- c("Botswana","Central African Republic","Congo","Kenya","Lesotho","Malawi",
-        "Namibia","South Africa","Swaziland","Uganda","Zambia","Zimbabwe")
+#2. "alpha" let us know that there are overlap data vitually 
+
+#3. sub date
+cc <- c("Botswana","Central African Republic","Congo","Kenya","Lesotho","Malawi", "Namibia","South Africa","Swaziland","Uganda","Zambia","Zimbabwe")
 hihiv <- filter(hiv,Country %in% cc)
-
-# Plotting new data
-ggplot(data = hihiv, mapping = aes(x = year, y= prevalence, group = Country, alpha = Country, color = Country)) + geom_line()
-
+  #Plotting new data
+ggplot(data = hiv, mapping = aes(x = year, y = prevalence, group_by = Country)) +
+  geom_line(alpha = 0.3) +
+  geom_line(data = hihiv, color = "red", alpha = 0.5)
--- Lab 3/Lab3.R
+++ Lab 3/Lab3.R
@@ -1,27 +1,37 @@
 
+#Importing
 library(tidyverse)
-hiv = read.csv("HIVprev.csv", stringsAsFactors = TRUE)
+hiv = read.csv("HIVprev.csv", stringsAsFactors = TRUE) #StringsAsFactors is not necessary
 hiv = select(hiv, Country, year, prevalence)
+
+#Virtuallizing
 head(hiv)
 tail(hiv)
 summary(hiv)
 view(hiv)
 
 #Exercise #1
-# Connect line between points
-ggplot(data = hiv, mapping = aes(x = year,y = prevalence, group_by = Country)) +
-  geom_line() + geom_point() +labs(title = "Estimated HIV Prevalence 1990-2000", y = "estimated prevalence")
+  # Connect line between points
+ggplot(data = hiv, mapping = aes(x = year,y = prevalence, group = Country, color = prevalence)) +
+  geom_line() + geom_point() + labs(title = "Estimated HIV Prevalence 1990-2000", y = "estimated prevalence")
+
 
 #Exercise #2
 # Show as curves instead
-ggplot(data = hiv, mapping = aes(x = year,y = prevalence, group_by = Country)) +
-  geom_smooth(color = "blue", se = FALSE) + labs(title = "Estimated HIV Prevalence 1990-2000", y = "estimated prevalence")
+ggplot(data = hiv, mapping = aes(x = year,y = prevalence, group = Country)) +
+  geom_smooth(se = FALSE, alpha = 0.1) + labs(title = "Estimated HIV Prevalence 1990-2000", y = "estimated prevalence")
+
 
 #Exercise #3
+
+
 cc <- c("Botswana","Central African Republic","Congo","Kenya","Lesotho","Malawi",
         "Namibia","South Africa","Swaziland","Uganda","Zambia","Zimbabwe")
 hihiv <- filter(hiv,Country %in% cc)
-ggplot(data = hiv, mapping = aes(y = prevalence, x = year, group_by = Country)) + 
-  geom_smooth(color = "grey", se = FALSE, alpha = 0.3) +
-  geom_smooth(data = hihiv, color = "red", se = FALSE, alpha = 0.3)
 
+ggplot(data = hiv, aes(y = prevalence, x = year)) + 
+  geom_line(aes(group = Country), color = "grey", alpha = 0.3) + 
+  geom_line(aes(group = Country), data = hihiv, color = "red", alpha = 0.3) +
+  geom_smooth(color = "black") +
+  geom_smooth(data = hihiv, color = "red") +
+  labs(title = "Estimated HIV Prevalence 1990-2000", y = "estimated prevalence", x = "year")
